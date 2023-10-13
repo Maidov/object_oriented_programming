a5dd80312ba99d3e2ef0aad9447bf265d97d8b0e
Югослав <115298811+Maidov@users.noreply.github.com>
2023-10-13 17:24:22 +0300

    Update funcky.cpp

--- lab1/funcky.cpp
+++ lab1/funcky.cpp
@@ -1,19 +1,30 @@
-#include "funcky.h"
-using namespace std;
+#include <iostream>
+#include <cmath>
 
-int funky(int a, int b)
-{
-    int total = 0;
-    for (int i = a; i < b + 1; i++)
-    {
-        int counter = 0;
-        int num = i;
-        while (num)
-        {
-            counter += (num % 2);
-            num /= 2;
-        }
-        total += counter;
+int countSetBits(int n) {
+    int count = 0;
+    while (n > 0) {
+        n = n & (n - 1);
+        count++;
     }
-    return total;
+    return count;
+}
+
+int count1From0to(int n) {
+    if (n > 0){
+        int firstPower = std::log2(n);
+        int digits = firstPower + 1;
+        int left_piece = n - pow(2, firstPower);
+        n -= (left_piece + 1);
+        
+        int power = std::log2(n);
+        int count = (pow(2, power + 1)/2)*(power + 1);
+        return count + 1 + left_piece + count1From0to(left_piece);
+    } else {
+        return 0;
+    }
+}
+
+int funky(int b, int a){
+    return count1From0to(a) - count1From0to(b) + countSetBits(b);
 }
