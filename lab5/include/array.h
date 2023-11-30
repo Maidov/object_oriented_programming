726e06c87e4c19e55afd00f3a659b725305808bb
master <heavenmaido@gmail.com>
2023-11-30 14:53:22 +0300

    Add remove(); Fixed class structure;

--- lab5/include/array.h
+++ lab5/include/array.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "allocator.h"
 
-// Динамический массив с параметризуемым аллокатором
+
 template <typename T, typename Allocator = PoolAllocator<T>>
 class Vector {
 public:
@@ -12,50 +12,18 @@ public:
     using size_type = std::size_t;
     using pointer = typename std::allocator_traits<Allocator>::pointer;
 
-    Vector() : data(nullptr), size(0), capacity(0), allocator() {}
+    Vector();
+    ~Vector();
 
-    ~Vector() {
-        clear();
-        v_deallocate();
-    }
+    size_type get_size() const;
+    size_type get_capacity() const;
 
-    void push_back(const T& value) {
-        if (size == capacity) {
-            // Если достигнут лимит capacity, увеличиваем его
-            if (capacity == 0) {v_reserve(1);}
-            else {v_reserve(capacity * 2);}
-        }
+    void push_back(const T& value);
+    void clear();
+    void remove(int index);
 
-        // Вызываем конструктор копирования для нового элемента
-        allocator.construct(data + size, value);
-        ++size;
-    }
-
-    size_type get_size() const {
-        return size;
-    }
-
-    size_type get_capacity() const {
-        return capacity;
-    }
-
-    void clear() {
-        for (size_type i = 0; i < size; ++i) {
-            // Вызываем деструктор для каждого элемента
-            allocator.destroy(data + i);
-        }
-        size = 0;
-    }
-
-    value_type& operator[](std::size_t index){
-        if ((index >= size) || (index < 0)) { throw std::out_of_range("Index out of range"); }
-        return data[index];
-    }
-
-    const value_type& operator[](std::size_t index) const{
-        if ((index >= size) || (index < 0)) { throw std::out_of_range("Index out of range"); }
-        return data[index];
-    }
+    value_type& operator[](std::size_t index);
+    const value_type& operator[](std::size_t index) const;
 
 
     class Iterator : public std::iterator<std::forward_iterator_tag, T>{
@@ -64,163 +32,297 @@ public:
         T *item;
     public:
         
-        Iterator(T* ptr) : item(ptr){}
+        Iterator(T* ptr);
+        Iterator(Iterator &other);
+        Iterator(Iterator &&other) noexcept;
+        ~Iterator();
 
-        Iterator(Iterator &other) : item(other.item) {}
+        bool operator==(const Iterator& other) const;
+        bool operator!=(const Iterator& other) const;
 
-        Iterator(Iterator &&other) noexcept {
-            item = std::move(other.item);
-        }
+        T& operator*() const;
+        T* operator->() const;
 
-        ~Iterator() = default;
-        bool operator==(const Iterator& other) const{
-            return item == other.item;
-        }
-
-        bool operator!=(const Iterator& other) const{
-            return !(item == other.item);
-        }
-
-        value_type& operator*() const{
-            return *item;
-        }
-
-        Iterator& operator++() {
-            ++item;
-            return *this;
-        }
-
-        Iterator operator++(int) {
-            Iterator temp = *this;
-            ++(*this);
-            return temp;
-        }
-
-        T* operator->() const{
-            return item;
-        }
-        
-        Iterator operator=(const Iterator& other) {
-            item = other.item;
-            return *this;
-        }
-
-        Iterator& operator =(Iterator &&other) noexcept{
-            item = std::move(other.item);
-            return *this;
-        }
-
+        Iterator& operator++();
+        Iterator operator++(int);
+        Iterator operator=(const Iterator& other);
+        Iterator& operator =(Iterator &&other) noexcept;
     };
     
 
     class ConstIterator : public std::iterator<std::forward_iterator_tag, T>{
         friend class List;
     private:
-        T *item;
+        const T *item;
     public:
         
-        ConstIterator(T* ptr) : item(ptr){}
+        ConstIterator(T* ptr);
+        ConstIterator(ConstIterator &other);
+        ConstIterator(ConstIterator &&other) noexcept;
+        ~ConstIterator();
 
-        ConstIterator(ConstIterator &other) : item(other.item) {}
+        bool operator==(const ConstIterator& other) const;
+        bool operator!=(const ConstIterator& other) const;
 
-        ConstIterator(ConstIterator &&other) noexcept {
-            item = std::move(other.item);
-        }
+        T& operator*() const;
+        T* operator->() const;
 
-        ~ConstIterator() = default;
-        bool operator==(const ConstIterator& other) const{
-            return item == other.item;
-        }
-
-        bool operator!=(const ConstIterator& other) const{
-            return !(item == other.item);
-        }
-
-        value_type& operator*() const{
-            return *item;
-        }
-
-        ConstIterator& operator++() {
-            ++item;
-            return *this;
-        }
-
-        ConstIterator operator++(int) {
-            ConstIterator temp = *this;
-            ++(*this);
-            return temp;
-        }
-
-        T* operator->() const{
-            return item;
-        }
-        
-        ConstIterator operator=(const ConstIterator& other) {
-            item = other.item;
-            return *this;
-        }
-
-        ConstIterator& operator =(ConstIterator &&other) noexcept{
-            item = std::move(other.item);
-            return *this;
-        }
-
+        ConstIterator& operator++();
+        ConstIterator operator++(int);
+        ConstIterator operator=(const ConstIterator& other);
+        ConstIterator& operator =(ConstIterator &&other) noexcept;
     };
 
+    Iterator begin() const;
+    Iterator end() const;
 
-    Iterator begin() const{
-        return(Iterator(data));
-    }
-
-    Iterator end() const{
-        return(Iterator(data + size));
-    }
-
-    ConstIterator cbegin() const{
-        return(ConstIterator(data));
-    }
-
-    ConstIterator cend() const{
-        return(ConstIterator(data + size));
-    }
+    ConstIterator cbegin() const;
+    ConstIterator cend() const;
 
 private:
     pointer data;          // Указатель на начало массива
-    size_type size;        // Текущий размер массива
-    size_type capacity;    // Емкость массива
-    PoolAllocator<T> allocator;  // Аллокатор
+    size_type size;
+    size_type capacity;
+    PoolAllocator<T> allocator;
 
-    void v_allocate(size_type new_capacity) {
-        data = allocator.allocate(new_capacity);
-        capacity = new_capacity;
-    }
-
-    void v_deallocate() {
-        if (data != nullptr){
-            allocator.deallocate(data, capacity);
-            data = nullptr;
-            capacity = 0;
-        }
-    }
-
-    void v_reserve(size_type new_capacity) {
-        if (new_capacity > capacity) {
-            
-            pointer new_data = allocator.allocate(new_capacity);
-            // Копируем существующие элементы в новый массив
-            for (size_type i = 0; i < size; ++i) {
-                
-                allocator.construct(new_data + i, std::move(data[i]));
-                allocator.destroy(data + i);
-            }
-            if (capacity != 0){allocator.deallocate(data, capacity);}
-            // Освобождаем старую память и обновляем указатель на новую
-            data = new_data;
-            capacity = new_capacity;
-        }
-    }
+    void v_allocate(size_type new_capacity);
+    void v_deallocate();
+    void v_reserve(size_type new_capacity);
 
 };
 
+template <typename T, typename Allocator>
+void Vector<T, Allocator>::remove(int index) {
+    if (index < 0 || index >= size) {
+        throw std::out_of_range("Index out of range");
+        return;
+    }
 
-// DONT PUT METHODS SEPARATELY COUZE USINGS WILL FAILED
+    data[index] = T();  // Заменяет элемент на значение по умолчанию для типа T
+    for (int i = index; i < size - 1; i++) {
+        data[i] = data[i + 1];
+    }
+    size--;
+}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::Vector() : data(nullptr), size(0), capacity(0), allocator() {}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::~Vector() {
+    clear();
+    v_deallocate();
+}
+
+template <typename T, typename Allocator>
+void Vector<T, Allocator>::push_back(const T& value) {
+    if (size == capacity) {
+        if (capacity == 0) {v_reserve(1);}
+        else {v_reserve(capacity * 2);}
+    }
+    allocator.construct(data + size, value);
+    ++size;
+}
+
+template <typename T, typename Allocator>
+size_t Vector<T, Allocator>::get_size() const {
+    return size;
+}
+
+template <typename T, typename Allocator>
+size_t Vector<T, Allocator>::get_capacity() const {
+    return capacity;
+}
+
+template <typename T, typename Allocator>
+void Vector<T, Allocator>::clear() {
+    for (size_type i = 0; i < size; ++i) {
+        allocator.destroy(data + i);
+    }
+    size = 0;
+}
+
+template <typename T, typename Allocator>
+T& Vector<T, Allocator>::operator[](std::size_t index){
+    if ((index >= size) || (index < 0)) { throw std::out_of_range("Index out of range"); }
+    return data[index];
+}
+
+template <typename T, typename Allocator>
+const T& Vector<T, Allocator>::operator[](std::size_t index) const{
+    if ((index >= size) || (index < 0)) { throw std::out_of_range("Index out of range"); }
+    return data[index];
+}
+
+template <typename T, typename Allocator>
+void Vector<T, Allocator>::v_allocate(size_type new_capacity) {
+    data = allocator.allocate(new_capacity);
+    capacity = new_capacity;
+}
+
+template <typename T, typename Allocator>
+void Vector<T, Allocator>::v_deallocate() {
+    if (data != nullptr){
+        allocator.deallocate(data, capacity);
+        data = nullptr;
+        capacity = 0;
+    }
+}
+
+template <typename T, typename Allocator>
+void Vector<T, Allocator>::v_reserve(size_type new_capacity) {
+    if (new_capacity > capacity) {
+        
+        pointer new_data = allocator.allocate(new_capacity);
+        for (size_type i = 0; i < size; ++i) {
+            
+            allocator.construct(new_data + i, std::move(data[i]));
+            allocator.destroy(data + i);
+        }
+        if (capacity != 0){allocator.deallocate(data, capacity);}
+        data = new_data;
+        capacity = new_capacity;
+    }
+}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::ConstIterator::ConstIterator(T* ptr) : item(ptr){}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::ConstIterator::ConstIterator(ConstIterator &other) : item(other.item) {}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::ConstIterator::ConstIterator(ConstIterator &&other) noexcept {
+    item = std::move(other.item);
+}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::ConstIterator::~ConstIterator() = default;
+
+template <typename T, typename Allocator>
+bool Vector<T, Allocator>::ConstIterator::operator==(const ConstIterator& other) const{
+    return item == other.item;
+}
+
+template <typename T, typename Allocator>
+bool Vector<T, Allocator>::ConstIterator::operator!=(const ConstIterator& other) const{
+    return !(item == other.item);
+}
+
+template <typename T, typename Allocator>
+T& Vector<T, Allocator>::ConstIterator::operator*() const{
+    return *item;
+}
+
+template <typename T, typename Allocator>
+T* Vector<T, Allocator>::ConstIterator::operator->() const{
+    return item;
+}
+
+
+
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::ConstIterator& Vector<T, Allocator>::ConstIterator::operator++() {
+    ++item;
+    return *this;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::ConstIterator Vector<T, Allocator>::ConstIterator::operator++(int) {
+    ConstIterator temp = *this;
+    ++(*this);
+    return temp;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::ConstIterator Vector<T, Allocator>::ConstIterator::operator=(const ConstIterator& other) {
+    item = other.item;
+    return *this;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::ConstIterator& Vector<T, Allocator>::ConstIterator::operator =(ConstIterator &&other) noexcept{
+    item = std::move(other.item);
+    return *this;
+}
+
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::Iterator::Iterator(T* ptr) : item(ptr){}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::Iterator::Iterator(Iterator &other) : item(other.item) {}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::Iterator::Iterator(Iterator &&other) noexcept {
+    item = std::move(other.item);
+}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::Iterator::~Iterator() = default;
+
+template <typename T, typename Allocator>
+bool Vector<T, Allocator>::Iterator::operator==(const Iterator& other) const{
+    return item == other.item;
+}
+
+template <typename T, typename Allocator>
+bool Vector<T, Allocator>::Iterator::operator!=(const Iterator& other) const{
+    return !(item == other.item);
+}
+
+template <typename T, typename Allocator>
+T& Vector<T, Allocator>::Iterator::operator*() const{
+    return *item;
+}
+
+template <typename T, typename Allocator>
+T* Vector<T, Allocator>::Iterator::operator->() const{
+    return item;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::Iterator& Vector<T, Allocator>::Iterator::operator++() {
+    ++item;
+    return *this;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::Iterator Vector<T, Allocator>::Iterator::operator++(int) {
+    Iterator temp = *this;
+    ++(*this);
+    return temp;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::Iterator Vector<T, Allocator>::Iterator::operator=(const Iterator& other) {
+    item = other.item;
+    return *this;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::Iterator& Vector<T, Allocator>::Iterator::operator =(Iterator &&other) noexcept{
+    item = std::move(other.item);
+    return *this;
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::Iterator Vector<T, Allocator>::begin() const{
+    return(Iterator(data));
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::Iterator Vector<T, Allocator>::end() const{
+    return(Iterator(data + size));
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::ConstIterator Vector<T, Allocator>::cbegin() const{
+    return(ConstIterator(data));
+}
+
+template <typename T, typename Allocator>
+typename Vector<T, Allocator>::ConstIterator Vector<T, Allocator>::cend() const{
+    return(ConstIterator(data + size));
+}
--- lab5/main.cpp
+++ lab5/main.cpp
@@ -20,9 +20,9 @@ int main() {
 
     // Используем Vector с аллокатором MyAllocator
     Vector<int> myVec;
-    const int g = 5;
+
     for (size_t i = 0; i < 10; ++i) {
-        myVec.push_back(g);
+        myVec.push_back(i);
     }
 
 
@@ -30,31 +30,40 @@ int main() {
         std::cout << *it << "\n";
     }
 
-    std::cout << "\n";
+    myVec.remove(2);
+    myVec.remove(2);
 
-    std::map<int, int, std::less<int>, PoolAllocator<std::pair<const int, int>>> test;
-    for (size_t i = 0; i < 10; ++i) {
-        test[i] = fact(i);
-    }
-
-    for (const auto &p : test) {
-        std::cout << "Key: " << p.first << " Value: " << p.second << '\n';
+    for (auto it = myVec.begin(); it != myVec.end(); it++){
+        std::cout << *it << "--\n";
     }
 
 
 
+    // std::cout << "\n";
 
-    // Используем функцию std::copy, ожидающую forward-итераторы
-    std::vector<int> destination;
-    std::copy(myVec.begin(), myVec.end(), std::back_inserter(destination));
-    // Выводим элементы из нового контейнера
-    std::cout << "Copied elements: \n";
-    for (const auto& element : destination) {
-        std::cout << element << " --\n";
-    }
-    std::cout << std::endl;
+    // std::map<int, int, std::less<int>, PoolAllocator<std::pair<const int, int>>> test;
+    // for (size_t i = 0; i < 10; ++i) {
+    //     test[i] = fact(i);
+    // }
 
-    std::cout << "\n";
-    std::cout << *std::find(myVec.begin(), myVec.end(), 6);
+    // for (const auto &p : test) {
+    //     std::cout << "Key: " << p.first << " Value: " << p.second << '\n';
+    // }
+
+
+
+
+    // // Используем функцию std::copy, ожидающую forward-итераторы
+    // std::vector<int> destination;
+    // std::copy(myVec.begin(), myVec.end(), std::back_inserter(destination));
+    // // Выводим элементы из нового контейнера
+    // std::cout << "Copied elements: \n";
+    // for (const auto& element : destination) {
+    //     std::cout << element << " --\n";
+    // }
+    // std::cout << std::endl;
+
+    // std::cout << "\n";
+    // std::cout << *std::find(myVec.begin(), myVec.end(), 5);
     return 0;
 }
